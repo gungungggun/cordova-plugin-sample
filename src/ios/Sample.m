5d2af7c7e0de9b4a176ecdb63c1cbe0789398ab7
gun <gungungggun@gmail.com>
2017-12-15 15:56:38 +0900

    first

---
+++ src/ios/Sample.m
@@ -0,0 +1,28 @@
+/********* Sample.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface Sample : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation Sample
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
